6b1f4142f07a9f8c8682fb7497130dc2c62e04db
NivOhavi <OhaviNiv@gmail.com>
2019-12-24 19:33:50 +0200

    - Added logo to app launch - Added indicatorView(spinner) in LoginViewController

--- Matkon/Controllers/LoginViewController.swift
+++ Matkon/Controllers/LoginViewController.swift
@@ -12,43 +12,51 @@ import FirebaseAuth
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     
-    @IBAction func loginAction(_ sender: Any) {
-        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
-           if error == nil{
-             self.performSegue(withIdentifier: "LoginToHome", sender: self)
-                          }
-            else{
-             let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                            
-              alertController.addAction(defaultAction)
-              self.present(alertController, animated: true, completion: nil)
-                 }
+    
+    
+    @IBAction func loginAction(_ sender: Any)
+    {
+        self.indicatorView.startAnimating()
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!)
+        { (user, error) in
+            if error == nil
+            {
+                self.performSegue(withIdentifier: "LoginToHome", sender: self)
+            }
+            else
+            {
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+                self.indicatorView.stopAnimating()
+            }
         }
-           
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        indicatorView.hidesWhenStopped = true
         
         
-
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Matkon/Controllers/SignUpViewController.swift
+++ Matkon/Controllers/SignUpViewController.swift
@@ -10,43 +10,48 @@ import UIKit
 import FirebaseAuth
 
 class SignUpViewController: UIViewController {
-
-
+    
+    
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var passwordConfirm: UITextField!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    
     
     
     @IBAction func signUpAction(_ sender: Any) {
         
         if password.text != passwordConfirm.text {
-        let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
-        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    
-        alertController.addAction(defaultAction)
-        self.present(alertController, animated: true, completion: nil)
-                }
-        else{
-        Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
-         if error == nil {
-           self.performSegue(withIdentifier: "SignupToLogin", sender: self)
-                        }
-         else{
-           let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
-           let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                            
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
             alertController.addAction(defaultAction)
             self.present(alertController, animated: true, completion: nil)
-               }
-                    }
-              }
+        }
+        else{
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                if error == nil {
+                    self.indicatorView.startAnimating()
+                    self.performSegue(withIdentifier: "SignupToLogin", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    self.indicatorView.stopAnimating()
+                }
+            }
+        }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.indicatorView.hidesWhenStopped = true
     }
     
-
+    
 }
