281b357d8e91e525b1f241070b36d03f1fcc55c3
Chen Amiel <chenamiell@gmail.com>
2020-01-31 12:48:50 +0200

    user already loogedin should transfer to categories

--- Matkon/Controllers/InitialViewController.swift
+++ Matkon/Controllers/InitialViewController.swift
@@ -12,21 +12,17 @@ class InitialViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        }
+    override func viewDidAppear(_ animated: Bool) {
+        if (ModelFirebaseAuth.instance.areUserLoggedIn()){
+            let tabBarVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "TabBarViewController")
+            tabBarVC.modalPresentationStyle = .fullScreen
+            self.present(tabBarVC, animated: true, completion: nil)
+            }
+    }
         
-
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+
